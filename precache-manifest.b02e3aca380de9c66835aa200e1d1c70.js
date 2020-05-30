6b1cc820b04a9f56d484e27e49191c71db57df28
AlexRozv <AlexRozv@gmail.com>
2020-05-30 19:51:09 +0300

    Updates

---
+++ precache-manifest.b02e3aca380de9c66835aa200e1d1c70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fce1777ae508aa1d4ff3c9f87ca7bf",
+    "url": "/Gravity/index.html"
+  },
+  {
+    "revision": "46e3f4ae9a371a268905",
+    "url": "/Gravity/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90a8e84e0a2d075d284f",
+    "url": "/Gravity/static/js/2.6ec0ead4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Gravity/static/js/2.6ec0ead4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e3f4ae9a371a268905",
+    "url": "/Gravity/static/js/main.60a2998e.chunk.js"
+  },
+  {
+    "revision": "e836d608c6068372674d",
+    "url": "/Gravity/static/js/runtime-main.e182e1a6.js"
+  },
+  {
+    "revision": "c73e6d9781659ca1cdc759173c57036b",
+    "url": "/Gravity/static/media/Placeholdervideo.c73e6d97.png"
+  },
+  {
+    "revision": "fd9e41f88197a87dbc31d52ca5fa59ce",
+    "url": "/Gravity/static/media/hero-img.fd9e41f8.png"
+  }
+]);
